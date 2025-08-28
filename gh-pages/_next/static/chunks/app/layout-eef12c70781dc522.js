3334627b21d5958608e68ec602887e41fdb32fc8
Guillem Perdigó Oliveras <guillem.perdigo@wbscodingschool.com>
2025-08-28 11:34:38 +0200

    Deploy static site with GitHub Pages base path configuration

---
+++ gh-pages/_next/static/chunks/app/layout-eef12c70781dc522.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3125:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,3673,23))},3673:()=>{}},e=>{e.O(0,[978,619,441,255,358],()=>e(e.s=3125)),_N_E=e.O()}]);
